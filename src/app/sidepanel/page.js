2385fd37183b631a7e53c3729c181af5975670d1
IamCocoDev <giovencofede@gmail.com>
2025-06-02 18:28:02 -0300

    Initial commit

---
+++ src/app/sidepanel/page.js
@@ -0,0 +1,41 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import {
+	meet,
+	MeetSidePanelClient,
+} from "@googleworkspace/meet-addons/meet.addons";
+import { CLOUD_PROJECT_NUMBER } from "@/constants";
+
+export default function Page() {
+	const [sidePanelClient, setSidePanelClient] = useState();
+
+	// Launches the main stage when the main button is clicked.
+	async function startActivity(e) {
+		if (!sidePanelClient) {
+			throw new Error("Side Panel is not yet initialized!");
+		}
+		await sidePanelClient.startActivity({
+			mainStageUrl: "http://localhost:3000/",
+		});
+	}
+
+	/**
+	 * Prepares the add-on Side Panel Client.
+	 */
+	useEffect(() => {
+		(async () => {
+			const session = await meet.addon.createAddonSession({
+				cloudProjectNumber: CLOUD_PROJECT_NUMBER,
+			});
+			setSidePanelClient(await session.createSidePanelClient());
+		})();
+	}, []);
+
+	return (
+		<>
+			<div>This is the add-on Side Panel. Only you can see this.</div>
+			<button onClick={startActivity}>Launch Activity in Main Stage.</button>
+		</>
+	);
+}
---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const CLOUD_PROJECT_NUMBER = "853013180742";
+export const MAIN_STAGE_URL =
+	"https://googleworkspace.github.io/meet/hello-world-next-js/mainstage";
