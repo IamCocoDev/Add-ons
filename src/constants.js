7c87fd7f269fd665e56dbeb5117e1f9f7fbed214
IamCocoDev <giovencofede@gmail.com>
2025-06-02 19:21:52 -0300

    Actualiza CLOUD_PROJECT_NUMBER en constants.js para incluir el ID del cliente de Google Cloud.

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,3 @@
-export const CLOUD_PROJECT_NUMBER = "853013180742";
+export const CLOUD_PROJECT_NUMBER =
+	"853013180742-mjkc6pgqrb84c6e8smp0qa9to36c16qp.apps.googleusercontent.com";
 export const MAIN_STAGE_URL = "https://iamcocodev.github.io/Add-ons/mainpanel";
